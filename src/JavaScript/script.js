b5603b65858bb9cc137e0f2067519d9d93447162
artjiyr <106913775+artjiyr@users.noreply.github.com>
2026-03-25 00:40:28 -0300

    slide quase finalizado

--- src/JavaScript/script.js
+++ src/JavaScript/script.js
@@ -2,38 +2,54 @@
 
 //MAPEAR AS COISAS Q SERÃO INTERATIVAS
 const latasMenores = document.querySelectorAll(".latas img:nth-child(2)");
-
 const slides = document.querySelectorAll(".slide");
 
 //contador 
 let contador = 0
 
-latasMenores.forEach(LataMenor=> {
+let clicar = true //TRUE -> PODE CLICAR //FALSE -> NÃO PODE CLICAR
+
+latasMenores.forEach((LataMenor) => {
     LataMenor.onclick = () => {
-        const slideAtivo = document.querySelector(".slide.active");
-        slideAtivo.classList.remove("active")
+        
 
-        //CONDICIONAIS IF e ELSE
+        if(clicar){
+            clicar = false
+            const slideAtivo = document.querySelector(".slide.active");
+            slideAtivo.classList.remove("active");
 
-        if(contador == 3){
-            contador = 0
-        } else {
-            contador = contador + 1
+            //CONDICIONAIS IF e ELSE
+
+            if(contador == 3){
+                contador = 0;
+            } else {
+                contador = contador + 1;
+            }
+
+            slides[contador].classList.add("active")
+            animarTitulo();
+
+            setTimeout(()=>{
+                clicar = true
+            }, 1000)
         }
 
         
-        slides[contador].classList.add("active")
         
     };
 });
 
-const split = SplitText.create(".conteudo h2", {
-    type: "chars",
-    mask: "chars"
-})
+function animarTitulo(){
+    const split = SplitText.create(".slide.active h2", {
+        type: "chars",
+        mask: "chars"
+    });
 
-gsap.from(split.chars, {
-    y: 80,
-    stagger: .05,
-    opacity: 0
-})
+    gsap.from(split.chars, {
+        y: 100,
+        stagger: .05,
+        duration: .5,
+        delay:.5,
+        opacity: 0
+    });
+}
